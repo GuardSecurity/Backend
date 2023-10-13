e27037ae63b3bba24dd9a0de16077159bdbeae9c
DungVC <dungvc.dev@gmail.com>
2023-10-13 23:54:01 +0700

    #feat: Implement login and signup

---
+++ Server/app.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
